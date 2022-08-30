fded46a86c7e3d5ccfcc5b694a272b282dd9e0db
sai nivas mangu <sainonbeat99@gmail.com>
2022-08-30 22:08:48 +0530

    add: code that doesn't work

--- wallpaper_functions.py
+++ wallpaper_functions.py
@@ -1,12 +1,24 @@
 from crud_functions import read_tasks
 
+import os
+
 # Function to create a wallpaper
 def create_wallpaper():
   # Get todo
-
+  task_list = read_tasks()
+  task_list = dict(task_list)
   # Put todo on image
+  wallpaper_text = ""
+  for key, value in task_list.items():
+    if value == 0:
+      wallpaper_text += "[ ] " + key + "\\n"
+    else:
+      wallpaper_text += "[X] " + key + "\\n"
 
-  print("Wallpaper Created")
+  create_wallpaper_command = "convert ./assets/base_image.png -gravity Center -font ./assets/fonts/JetBrainsMono.ttf -pointsize 30 -fill #cdd6f4 -annotate 0 '{}' ./assets/todowalp.png".format(wallpaper_text)
+
+  print(create_wallpaper_command)
+  # os.system(create_wallpaper_command)
 
 def set_wallpaper():
   # Set wallpaper
