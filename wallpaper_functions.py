16cf28e7d9b6c628750608d97d63e5a1f1046aae
sai nivas mangu <sainonbeat99@gmail.com>
2022-08-30 19:05:36 +0530

    add: wallpaper functions

---
+++ wallpaper_functions.py
@@ -0,0 +1,16 @@
+from crud_functions import read_tasks
+
+# Function to create a wallpaper
+def create_wallpaper():
+  # Get todo
+
+  # Put todo on image
+
+  print("Wallpaper Created")
+
+def set_wallpaper():
+  # Set wallpaper
+
+  print("Wallpaper set")
+
+create_wallpaper()
